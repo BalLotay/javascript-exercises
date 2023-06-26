e6ac871fbdbc306ad985851fd7018fb56ab9cdaa
Balpreet Singh <singhbalpreet007@gmail.com>
2023-06-26 15:20:21 -0400

    Edit sumAll.js and sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
 
+    let start = a;
+    let end = b;
+
+    if (a > b) {
+        start = b;
+        end = a;
+    } 
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
