b28b802115d525bc5778c63b518dbac00fd64b6c
Balpreet Singh <singhbalpreet007@gmail.com>
2023-06-26 13:08:59 -0400

    Edit removeFromArray.js and removeFromArray.spec.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (const arg of args) {
+        if (array.includes(arg)) {
+            let index = array.indexOf(arg);
+            array.splice(index, 1); 
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
